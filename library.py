6dc3d30b4bbb7374f600df0fd775da402725f756
joaquin <joaquin.bejar@gmail.com>
2018-09-03 10:29:11 +0200

    First working version of Anomaly Detection and Clustering for User classification

---
+++ library.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+import matplotlib.pyplot as plt
+#from pandas.tools.plotting import scatter_matrix
+from pandas.plotting import scatter_matrix
+
+import collections
+import numpy as np
+
+import dill
+
+host = '10.0.5.225'
+db = 'billin'
+user = 'postgres'
+password = 'ppoSgre123'
+
